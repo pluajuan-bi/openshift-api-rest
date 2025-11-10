30fdded7e26dc75752d0dade16dab8fb6376d47c
pluaj <plua.juan@fisagrp>
2025-11-10 14:33:45 -0500

    first commit

---
+++ src/main/java/com/example/demo/Usuario.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "usuario")
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombre;
+    private String email;
+
+    public Usuario() {}
+
+    public Usuario(String nombre, String email) {
+        this.nombre = nombre;
+        this.email = email;
+    }
+
+    public Long getId() { return id; }
+    public String getNombre() { return nombre; }
+    public String getEmail() { return email; }
+
+    public void setNombre(String nombre) { this.nombre = nombre; }
+    public void setEmail(String email) { this.email = email; }
+}
---
+++ target/classes/schema.sql
@@ -0,0 +1,6 @@
+
+CREATE TABLE IF NOT EXISTS usuario (
+    id SERIAL PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    email VARCHAR(150) NOT NULL
+    );
